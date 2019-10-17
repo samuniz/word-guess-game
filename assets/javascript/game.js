ceae4b80df0cdebfb3164ef7cbac00897f8128ba
Sabrina <sabrina.muniz@icloud.com>
2019-10-17 10:08:41 -0400

    added conditional  statements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,41 @@
 var words = ["jupiter", "comet", "star", "moon", "sky", "pluto", "eclipse", "nebula", "supernova", "zodiac", "galaxy"];
-var score 
-var wordGuessed = 0;
+var score; 
+var letterGuessed = 0;
+var computerChoice;
 //user press any key to start the game
 document.onkeyup = function(event){
     //computer pick a word
-     var computerChoice = words[0]; 
-    
-    //save the user guess 
-     var userGuess = event.key;
-    
+    computerChoice = words[0]; 
+};
+//user press a key to choose a word from the computer choice
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+      //add to lettterGuessed if the letter matches any letter on computerChoice
+      if (computerChoice.indexOf(userGuess) == -1 ){
+          // if letter that user choose don't exist in the computer choice string it adds 1;  
+        console.log(letterGuessed=+1);
+               }
+               else {
+                   //score goes up
+                   score++; 
+                   //display the letter on the screen
+               }; 
+    //when the letter Guessed reach 10 game stop            
+      if (letterGuessed > 11){
+          alert("Game Over !");
+      };
+      
+      
+}
+console.log(letterGuessed); 
      
-     console.log(computerChoice); 
-     console.log(userGuess); 
-     //add to wordGuessed if the letter matches any letter on computerChoice
-     if (computerChoice.indexOf(userGuess) !== -1 ){
-     wordGuessed++ ;
-            }
+   
        
-       
-        };
-     
+  //Why my letterGuessed variable is not saving 1 whenn function runs ?
+  //should I put the function inside a while loop ?
+  //How can I display the length of computer choice as _ and when user guess the right lettter review the letter guessed ?
+
+        
 
 
 
