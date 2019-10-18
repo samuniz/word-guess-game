bbc9ddc6547b908c7fe9b7137755782dec9e69fe
Sabrina <sabrina.muniz@icloud.com>
2019-10-17 21:53:44 -0400

    added wrong guess conditional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var words = ["jupiter", "comet", "star", "moon", "sky", "pluto", "eclipse", "nebula", "supernova", "zodiac", "galaxy"];
 var score; 
-var wrongWord = [];
+var wrongGuess = [];
 var wrongTries = 0;
 var userGuess = [];
 var wordToGuess = [];
@@ -12,34 +12,34 @@ var rightWord = [];
 var randomWord = words[Math.floor(Math.random()* words.length)]
     console.log(randomWord); 
     for (var i = 0; i < randomWord.length; i++){
-        // console.log(randomWord[i]); 
         //create the _ in the page 
         wordToGuess.push(" _ "); 
-        // console.log(wordToGuess)
-        // console.log(wordToGuess);
     }
 
     //user press a key and key is saved on userGuess variable
     document.onkeyup = function (event) {
         var userGuess = event.key.toString();
         
-        // console.log(userGuess);
-         
+     
+     //compare the user guess with random word    
         if (randomWord.indexOf(userGuess) >-1) {
+            //if user guess is correct add the letter to variable rightWord 
              rightWord.push(userGuess); 
 
-            // console.log(rightWord);
-            // replace _ with letter 
+            // replace _ with correspondent letter 
             wordToGuess[randomWord.indexOf(userGuess)] = userGuess; 
-            console.log(wordToGuess);
-
-
+            // console.log(wordToGuess)
+            if (randomWord.length == rightWord.length) {
+                score++    
+            }
         } 
         else {
-            wrongWord.push(userGuess);
-            // console.log(wrongWord);
+            //add wrong guess to var
+            wrongGuess.push(userGuess);  
+            if (wrongGuess.length >10){
+                alert("You lost!");}
         }
-        wordToGuess
+    
       
 }
 
